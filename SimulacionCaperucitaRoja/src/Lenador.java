3101a2e18ff0377b4052107c8701ed2dd0af34d1
PeterSolidSnake <pedrosantiagois@ufps.edu.co>
2026-04-13 23:55:47 -0500

    Started the project

--- SimulacionCaperucitaRoja/src/Lenador.java
+++ SimulacionCaperucitaRoja/src/Lenador.java
@@ -1,2 +1,14 @@
 public class Lenador {
+
+    private boolean hacha;
+
+    public Lenador(boolean hacha) {
+        this.hacha = hacha;
+    }
+
+    public boolean atacar(){
+
+        return  false;
+    }
+
 }
